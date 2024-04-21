5aba04e09ceaccb9d237881e42bb0462018dc177
murarimishra2004 <muraripcmb.1@gmail.com>
2024-04-21 14:52:45 +0530

    Add realstatewebsite folder

---
+++ chatgpt/src/App.js
@@ -0,0 +1,21 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className='sidebar'>
+        <div className='Upperside'>
+
+        </div>
+        .lowerside
+        
+       </div>
+       <div className="main">
+
+       </div>
+    </div>
+  );
+}
+
+export default App;
